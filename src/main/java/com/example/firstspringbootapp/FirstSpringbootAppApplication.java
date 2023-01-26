5d5e764d4c31dc0064be9ce956865708c49abb4a
Deepak_Dev <deepak.devdd.97@gmail.com>
2023-01-26 15:27:20 +0530

    Initial commit

---
+++ src/main/java/com/example/firstspringbootapp/FirstSpringbootAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringbootAppApplication.class, args);
+	}
+
+}
